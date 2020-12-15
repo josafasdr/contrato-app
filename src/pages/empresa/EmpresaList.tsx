b388cc97b19612a8e7d437af30162d5424bd6efb
Josafá Santos <josafa.s.reis@gmail.com>
2020-12-15 20:58:35 -0300

    Criação da tabela de empresas

--- src/pages/empresa/EmpresaList.tsx
+++ src/pages/empresa/EmpresaList.tsx
@@ -1,17 +1,132 @@
-import { makeStyles } from '@material-ui/core'
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import {
+  makeStyles,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableSortLabel,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow
+} from '@material-ui/core'
+import useService from '../../hooks/useService'
+
+const columns = [
+  // { id: 'id', label: 'Id', minWidth: 50 },
+  { id: '_id', label: 'Id' },
+  { id: 'nomeFantasia', label: 'Nome' },
+  { id: 'rasaoSocial', label: 'Razão Social' },
+  { id: 'cnpj', label: 'CNPJ' },
+  { id: 'email', label: 'E-mail' }
+];
 
 const useStyles = makeStyles({
   root: {
-    //display: 'flex'
+    width: '100%'
+  },
+
+  button: {
+    margin: '10px 0 10px 0',
+    textTransform: 'none'
+  },
+
+  container: {
+    maxHeight: '73vh'
   }
 })
 
 const EmpresaList = () => {
   const classes = useStyles()
+  const { loading, data, error } = useService({
+    url: 'http://localhost:4000/empresas',
+    //url: 'https://contrato-api.herokuapp.com/empresas',
+    method: 'GET'
+  })
+
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+
+  const handleChangePage = (_: any, newPage: any) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = (event: any) => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
 
   return (
     <div className={classes.root}>
-      <h3>Empresa List</h3>
+      <Link to={`/empresas/create`}>
+        <Button
+          className={classes.button}
+          variant="contained"
+          size="small"
+          color="primary"
+        >
+          Inserir Empresa
+        </Button>
+      </Link>
+      {
+        loading ? 
+          <p>Carregando...</p> : 
+        error ? 
+          <p>Erro :(</p> :
+        <Paper className={classes.root}>
+          <TableContainer className={classes.container}>
+            <Table stickyHeader={true} aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column: any) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ backgroundColor: '#f5f5f5' }}
+                    >
+                      {column.label}
+                      <TableSortLabel />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row: any) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row._id}>
+                      {/* <TableCell>{new Date(parseInt(row.createdAt)).toLocaleDateString('pt-br')}</TableCell> */}
+                      <TableCell>{row._id}</TableCell>
+                      <TableCell>{row.nomeFantasia}</TableCell>
+                      <TableCell>{row.razaoSocial}</TableCell>
+                      <TableCell>{row.cnpj}</TableCell>
+                      <TableCell>{row.email}</TableCell>
+                      {/* <TableCell>
+                        <Link to={`/empresas/${row._id}`}>
+                          <FileCopyIcon />
+                        </Link>
+                      </TableCell> */}
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            labelRowsPerPage="Linhas por página"
+            rowsPerPageOptions={[10, 25, 100]}
+            labelDisplayedRows={({ from, to, count }) => `${from}-${to} de ${count}`}
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        </Paper>
+      }
     </div>
   )
 }
