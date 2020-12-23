32c2312835c38476044b8c7b37fd966878446d52
Gabriel Dias <dias.1618@gmail.com>
2020-12-23 08:51:47 -0300

    feito abas no Home com os tres relatorios

---
+++ src/components/relatorios/RelatorioAditivos.tsx
@@ -0,0 +1,82 @@
+import { FormControl, Input, InputLabel, makeStyles, MenuItem, Paper, Select } from "@material-ui/core"
+import useService from "../../hooks/useService";
+
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      [theme.breakpoints.up('sm')]: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+      }
+    },
+  
+    margin: {
+      margin: theme.spacing(1),
+    },
+  
+    grid: {
+      width: '100%'
+    },
+  
+    textField: {
+      marginTop: '30px',
+      [theme.breakpoints.up('sm')]: {
+        marginRight: '10px'
+      }
+    },
+  
+    flexGrow1: {
+      flexGrow: 1
+    }
+}))
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+export const RelatorioAditivos = () => {
+    const classes = useStyles()
+    const { loading, data, error } = useService({
+      url: `${process.env.REACT_APP_PATH_API}/empresas`,
+      method: 'GET'
+    })
+    
+    const handleChange = (event: any) => {
+      const { name, value } = event.target;
+    }
+  
+    return (
+        <Paper>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="empresa-label">Empresa</InputLabel>
+              <Select 
+                labelId="empresa-label"
+                id="empresa"
+                name="_idEmpresa"
+                input={<Input />}
+                MenuProps={MenuProps}
+                onChange={handleChange}>
+                  {data.map((empresa:any) => (
+                    <MenuItem key={empresa._id} value={empresa._id}>
+                      {empresa.razaoSocial}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+        </Paper>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,51 @@
-import { makeStyles } from '@material-ui/core'
+import { AppBar, makeStyles, Paper, Tab, Tabs } from '@material-ui/core'
+import { useState } from 'react';
 
 import { RelatorioPagamento } from '../components/relatorios'
+import { RelatorioAditivos } from '../components/relatorios/RelatorioAditivos';
+import { RelatorioContratos } from '../components/relatorios/RelatorioContratos';
+import { TabPanel } from '../components/TabPanel';
 
 const useStyles = makeStyles({
   root: {
   }
 })
 
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const Home = () => {
   const classes = useStyles()
+  const [value, setValue] = useState(0);
+  
+  const tabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <div className={classes.root}>
-      <RelatorioPagamento />
+      <Paper>
+        <AppBar position="static">
+          <Tabs value={value} onChange={tabChange} aria-label="simple tabs example">
+            <Tab label="Contratos" {...a11yProps(0)} />
+            <Tab label="Aditivos" {...a11yProps(1)} />
+            <Tab label="Pagamentos" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <RelatorioPagamento />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <RelatorioAditivos />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <RelatorioContratos />
+        </TabPanel>
+      </Paper>
     </div>
   )
 }
